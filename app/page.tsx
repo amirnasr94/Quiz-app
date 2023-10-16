32ac4c6fbda9c4800f90ea06dc16d22a4a132bb3
amirnasr <amir.nasr72@gmail.com>
2023-10-16 10:53:00 +0330

    some changes in global.css & page.tsx in root dir

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Link from "next/link";
 
 
@@ -8,7 +7,7 @@ export default function Home() {
       <h1 className='text-4xl text-gray-700'>Quiz App</h1>
       <div className='w-full p-10 m-auto text-center'>
         <Link href="/quiz">
-          <button className='w-2/6 rounded-lg bg-cyan-900 border-cyan-900 py-5 m-auto text-white hover:bg-cyan-700'>شروع سوالات</button>
+          <button className='w-2/6 rounded-lg bg-cyan-900 border-cyan-900 py-5 m-auto text-white hover:bg-cyan-700'>Start</button>
         </Link>
       </div>
     </main>
