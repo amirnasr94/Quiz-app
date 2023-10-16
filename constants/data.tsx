a53ab6cc57cc3b25617ec80b6350dd926ff3ed72
amirnasr <amir.nasr72@gmail.com>
2023-10-16 10:49:53 +0330

    initial data and create modal for types

---
+++ constants/data.tsx
@@ -0,0 +1,34 @@
+import { Question } from "./modal";
+
+export const question:Question[] = [
+    {
+        id:1,
+        title:"The number of Olympic rings?",
+        answer:["5","4","6","8"],
+        correctAnswer:"5"
+    },
+    {
+        id:2,
+        title:"Who is the work of Jekund's smile painting?",
+        answer:["Vengok","Davinci","Picasso","Rembrandt"],
+        correctAnswer:"Davinci"
+    },
+    {
+        id:3,
+        title:"How many years did the Hundred Years War last?",
+        answer:["97 years","100 years","116 years","121 years"],
+        correctAnswer:"116 years"
+    },
+    {
+        id:4,
+        title:"Which book is not by Victor Hugo?",
+        answer:["The Poor","White Fang","human soul","A man who laughs"],
+        correctAnswer:"White Fang"
+    },
+    {
+        id:5,
+        title:"How many houses are there on the chessboard?",
+        answer:["49","81","36","64"],
+        correctAnswer:"64"
+    }
+]
